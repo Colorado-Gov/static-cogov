bceb3be027855254649b558c93a71a010a97c460
alne1147 <alne1147@live.com>
2016-11-02 19:00:25 -0600

    added contact us and online services page.

--- js/load.js
+++ js/load.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
     $('.sa-link').click(function(){
         $('#home').load('state-agencies.html');
     });
+    
+    $('.contact').click(function(){
+        $('#home').load('contact-us.html');
+    });
+    
+    $('.os-link').click(function(){
+        $('#home').load('online-services.html');
+    });
 })
